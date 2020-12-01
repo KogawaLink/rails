e209f799a1891c20cc239a18d67c6da7165b3e23
Kogawa <utyuu1007@yahoo.co.jp>
2020-12-01 14:53:02 +0000

    rails

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'users/new'
+  get 'pages/index'
+  root 'pages#index'
+  
+  root 'pages#index'
+  get 'pages/help'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+  get 'pages/help'
+  
+  resources :users
+end
