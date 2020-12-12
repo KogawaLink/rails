a85a2fba901bc43be313148c467e88ad81683861
Kogawa <utyuu1007@yahoo.co.jp>
2020-12-12 14:46:26 +0000

    課題用

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: { with: /\w+@[a-z\d]+\.\w/ }
+  validates :password, length: { in: 8..32 }, presence: true,format: { with: /(?=.*?[a-zA-Z])(?=.*?[\d])/ }
+  has_secure_password
 end
